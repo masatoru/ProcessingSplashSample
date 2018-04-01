da0b213a444651e9510d82d071dc520470f6dc41
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-04-01 09:18:23 +0900

    add ProcessingSplashSample

---
+++ ProcessingSplashSample/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProcessingSplashSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// https://qiita.com/dhq_boiler/items/930ff2dc960fd950d7a3
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            ProcessingSplash ps = new ProcessingSplash("Initializing", () =>
+            {
+                //ここで重い処理を処理させる
+                System.Threading.Thread.Sleep(3000);
+            });
+
+            Console.WriteLine("ShowDialog 1");
+            //バックグラウンド処理が終わるまで表示して待つ
+            ps.ShowDialog();
+
+            if (ps.complete)
+            {
+                Console.WriteLine("処理が成功した");
+            }
+            else
+            {
+                Console.WriteLine("処理が失敗した");
+            }
+        }
+    }
+}
